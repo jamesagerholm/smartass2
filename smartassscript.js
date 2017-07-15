dd6d71cf883fc4f6e0c9dad886cd37c48808d8ee
James Agerholm <jamesag2000@gmail.com>
2017-07-15 13:22:42 +0100

    Go

---
+++ smartassscript.js
@@ -0,0 +1,50 @@
+
+
+$(document).ready(function(){
+
+$(".list").hide('slow');
+
+$("p, h2").mouseover(function(){
+    $(this).css("color","blue"); 
+});  
+
+$("li").mouseover(function(){
+    $(this).css("color","blue");
+});  
+
+$("li").mouseout(function(){
+    $(this).css("color","black");
+});  
+
+
+$("p, h2  ").mouseout(function(){
+    $(this).css("color","black");
+});
+
+$(".listbutton, .boxbutton").click(function(){
+    $(".list").slideToggle("350");
+});
+$('#btn').on('click', function() {
+  $('#msg').append(' World');
+});
+
+// jQuery
+$('#btn').on('click', function() {
+  $('#msg').append(' World');
+});
+
+$('#about')
+
+$('controls').remove(".smart")
+
+});
+
+// $(function(){
+// 	var x = 0;
+// 	setInterval(function(){
+// 		x=-1;
+// 		$('')
+// }
+
+
+
